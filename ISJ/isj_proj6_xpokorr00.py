d2b002b2d130618ee35cf5399cb9ba511821341d
RadimPokorny19 <98946296+RadimPokorny@users.noreply.github.com>
2025-04-03 09:35:38 +0200

    Add ISJ proj6 beginning

---
+++ ISJ/isj_proj6_xpokorr00.py
@@ -0,0 +1,7 @@
+#!/usr/bin/env python3
+
+import collections
+
+def upd_word_counts(sentence: str, word_counts: collections.Counter, to_upper: bool) -> collections.Counter:
+    
+    return word_counts
