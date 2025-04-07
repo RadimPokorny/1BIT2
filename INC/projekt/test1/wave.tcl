a87aa266df3998642d18b26b73f6e54087a8c40e
RadimPokorny19 <98946296+RadimPokorny@users.noreply.github.com>
2025-04-07 13:26:01 +0200

    Add ITW proj2 navbar init

--- INC/projekt/test1/wave.tcl
+++ INC/projekt/test1/wave.tcl
@@ -6,10 +6,11 @@ gtkwave::addSignalsFromList [list clk rst]
 gtkwave::addSignalsFromList [list din_state]
 gtkwave::/Edit/Color_Format/Blue
 gtkwave::addSignalsFromList [list din dout dout_vld]
+gtkwave::addSignalsFromList [list dout_exp dout_error]
+gtkwave::/Edit/Color_Format/Blue
 gtkwave::/Edit/Insert_Blank
 
 gtkwave::/Edit/Insert_Comment "Other Signals"
-         # <<< TODO: Insert 'gtkwave::addSignalsFromList' for your signals here.
 
 gtkwave::/Edit/UnHighlight_All
 gtkwave::/Time/Zoom/Zoom_Best_Fit
