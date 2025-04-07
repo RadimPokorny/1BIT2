16b34938ab96159b48ccf30016a1618c18175d5a
RadimPokorny19 <98946296+RadimPokorny@users.noreply.github.com>
2025-04-07 10:26:22 +0200

    Add ISJ proj6 done

--- ISJ/isj_proj6_xpokorr00.py
+++ ISJ/isj_proj6_xpokorr00.py
@@ -1,7 +1,31 @@
 #!/usr/bin/env python3
 
 import collections
+from collections import Counter
 
-def upd_word_counts(sentence: str, word_counts: collections.Counter, to_upper: bool) -> collections.Counter:
+class ValidationError(Exception):
+    pass
+
+def upd_word_counts(sentence: str, word_counts: collections.Counter = None, *, to_upper: bool = False) -> collections.Counter:
+
+    if word_counts is None:
+        word_counts = collections.Counter()
     
-    return word_counts
+    # Variable to store the copy of the collection
+    result = collections.Counter(word_counts)
+    
+    if to_upper:
+        # If Upper then checking if the values are valid
+        for word in word_counts:
+            if not word.isupper():
+                raise ValidationError("Invalid combination of Uppercase and Lowercase!")
+        
+        # Split a string to the array
+        words = sentence.upper().split()
+    else:
+        words = sentence.split()
+    
+    # Update the counter
+    result.update(words)
+    
+    return result
